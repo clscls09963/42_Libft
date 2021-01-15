dccd86f2f56f24b5a45dc947c914584d4f5f0f93
clscls09963 <clscls2530@naver.com>
2021-01-15 18:32:25 +0900

    libft

---
+++ ft_substr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyeykim <hyeykim@student.42seoul.k>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/13 03:32:34 by hyeykim           #+#    #+#             */
+/*   Updated: 2021/01/13 04:28:32 by hyeykim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char		*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t	size;
+
+	if (s == 0)
+		return ((void *)0);
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (str == 0)
+		return ((void *)0);
+	size = ft_strlen(s);
+	if (size <= start)
+	{
+		str[0] = 0;
+		return (str);
+	}
+	ft_strlcpy(str, s + start, len + 1);
+	return (str);
+}
